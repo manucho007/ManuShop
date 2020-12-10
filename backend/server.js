c50b2f677ceb2f0bfab971c59c957004e9489881
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-12-11 00:48:51 +0300

    Added env file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
 const app = express();
 
+dotenv.config();
 // Routes
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -18,4 +20,7 @@ app.get('/api/products/:id', (req, res) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
