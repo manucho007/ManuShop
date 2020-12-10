c239dc52615e4c9f90f06ceb2ff59fdef507bbb7
Manuel Rodriguez <Manuel.Rodriguez@payever.org>
2020-12-10 11:55:25 +0300

    Created simple express response

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const products = require('./data/products');
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
