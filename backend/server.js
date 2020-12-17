9b38d610a5af4738a3757a03da97c25ef666d43c
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-17 13:02:10 +0300

    Added login route, model and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
@@ -13,6 +14,9 @@ dotenv.config();
 // Connection to DB
 connectDB();
 
+// Parser
+app.use(express.json());
+
 // API running
 app.get('/', (req, res) => {
   res.send('API is running');
@@ -20,6 +24,7 @@ app.get('/', (req, res) => {
 
 // Route mounts
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // ***************Middleware
 // Deal with 404
