06f7bf0c1c2722e63b5cd09ab02ac3e246954e6a
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-19 18:31:54 +0300

    Added login screen and localStorage for user

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,16 +16,19 @@ export const login = (email, password) => async (dispatch) => {
         'Content-Type': 'application/json',
       },
     };
+
     const { data } = await axios.post(
       '/api/users/login',
       { email, password },
       config
     );
+
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
     });
-    localStorage.setItem('userInfo', JSON.stringify('data'));
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
