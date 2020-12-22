d45291c5216d0b64e4a4a8040009352eb270816b
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-22 12:57:30 +0300

    added breadcrumbs fro checkout steps

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className='justify-content-center mb-4'>
+      <Nav.Item>
+        {step1 ? (
+          <Nav.Link to='/login'>Sign In</Nav.Link>
+        ) : (
+          <Nav.Link disabled>Sign In</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step2 ? (
+          <Nav.Link to='/shipping'>Shipping</Nav.Link>
+        ) : (
+          <Nav.Link disabled>Shipping</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step3 ? (
+          <Nav.Link to='/payment'>Payment</Nav.Link>
+        ) : (
+          <Nav.Link disabled>Payment</Nav.Link>
+        )}
+      </Nav.Item>
+      <Nav.Item>
+        {step4 ? (
+          <Nav.Link to='/placeorder'>Place Order</Nav.Link>
+        ) : (
+          <Nav.Link disabled>Place Order</Nav.Link>
+        )}
+      </Nav.Item>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;
