718337113065cc7a1d3267c7d5389cc99e37dad2
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-23 11:46:25 +0300

    Fixed links in navigation

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
   return (
     <Nav className='justify-content-center mb-4'>
       <Nav.Item>
         {step1 ? (
-          <Nav.Link to='/login'>Sign In</Nav.Link>
+          <LinkContainer to='/login'>
+            <Nav.Link>Sign In</Nav.Link>
+          </LinkContainer>
         ) : (
           <Nav.Link disabled>Sign In</Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step2 ? (
-          <Nav.Link to='/shipping'>Shipping</Nav.Link>
+          <LinkContainer to='/shipping'>
+            <Nav.Link>Shipping</Nav.Link>
+          </LinkContainer>
         ) : (
           <Nav.Link disabled>Shipping</Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step3 ? (
-          <Nav.Link to='/payment'>Payment</Nav.Link>
+          <LinkContainer to='/payment'>
+            <Nav.Link>Payment</Nav.Link>
+          </LinkContainer>
         ) : (
           <Nav.Link disabled>Payment</Nav.Link>
         )}
       </Nav.Item>
       <Nav.Item>
         {step4 ? (
-          <Nav.Link to='/placeorder'>Place Order</Nav.Link>
+          <LinkContainer to='/placeorder'>
+            <Nav.Link>Place Order</Nav.Link>
+          </LinkContainer>
         ) : (
           <Nav.Link disabled>Place Order</Nav.Link>
         )}
