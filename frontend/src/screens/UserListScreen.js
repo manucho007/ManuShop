85ebaf6916a7879dba15ad975a41ba609c3efa66
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-24 13:46:41 +0300

    Added backend, store action reducer for deleteUser

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { Table, Button } from 'react-bootstrap';
-import { listUsers } from '../actions/userActions';
+import { listUsers, deleteuser } from '../actions/userActions';
 const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
@@ -14,16 +14,21 @@ const UserListScreen = ({ history }) => {
   //   Get current logged user from state
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
       dispatch(listUsers());
     } else {
       history.push('login');
     }
-  }, [dispatch, userInfo, history]);
+  }, [dispatch, userInfo, history, successDelete]);
 
   const deleteHandler = (id) => {
-    console.log(`delete ID ${id}`);
+    if (window.confirm('Are you sure?')) {
+      dispatch(deleteuser(id));
+    }
   };
   return (
     <>
