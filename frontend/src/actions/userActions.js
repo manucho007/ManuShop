0f5be783c5be145a61e36d58ad386b0f3a5fd03e
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-24 13:19:28 +0300

    Fixed security issues on admin logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -17,6 +17,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from '../constants/userConstants';
 import { ORDER_LIST_RESET } from '../constants/orderConstants';
 export const login = (email, password) => async (dispatch) => {
@@ -197,4 +198,5 @@ export const logout = () => async (dispatch) => {
   dispatch({ type: USER_LOGOUT });
   dispatch({ type: USER_DETAILS_RESET });
   dispatch({ type: ORDER_LIST_RESET });
+  dispatch({ type: USER_LIST_RESET });
 };
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -5,15 +5,22 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { Table, Button } from 'react-bootstrap';
 import { listUsers } from '../actions/userActions';
-const UserListScreen = () => {
+const UserListScreen = ({ history }) => {
   const dispatch = useDispatch();
+
   // Getting the list of users from state
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
-
+  //   Get current logged user from state
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
   useEffect(() => {
-    dispatch(listUsers());
-  }, [dispatch]);
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      history.push('login');
+    }
+  }, [dispatch, userInfo, history]);
 
   const deleteHandler = (id) => {
     console.log(`delete ID ${id}`);
