88c29df7821c92f5a37f72fa783adbd046260bb3
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-24 14:15:04 +0300

    Added routes and controller get/update user(admin)

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,6 +129,46 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get user by id
+// @route   Get /api/users/:id
+// @access  Private/Admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+// @desc    UPDATE user
+// @route   PUT /api/users/:id
+// @access  Private/Admin
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  // We check if the user exists
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('Not found');
+  }
+});
+
 export {
   authUser,
   getUserProfile,
@@ -136,4 +176,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUser,
 };
