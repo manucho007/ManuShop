4a3babacf09436f2adf76c8174780e284a6bb699
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-28 23:09:36 +0300

    added backend for uploading images with mulder

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const app = express();
@@ -27,6 +29,7 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 // Paypal config
 app.get('/api/config/paypal', (req, res) =>
@@ -39,6 +42,10 @@ app.use(notFound);
 
 // Custom error handler
 app.use(errorHandler);
+
+// make uploads folder static
+const __dirname = path.resolve(); //__dirname is not available in ES6 modules so we use this
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
