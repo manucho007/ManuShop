e1893837b344b5f4740585e80ada742b3d3cc6c8
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-28 13:11:04 +0300

    Added productlistscreen

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -3,36 +3,46 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { Table, Button } from 'react-bootstrap';
-import { listUsers, deleteuser } from '../actions/userActions';
-const UserListScreen = ({ history }) => {
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { listProducts } from '../actions/productActions';
+const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch();
 
-  // Getting the list of users from state
-  const userList = useSelector((state) => state.userList);
-  const { loading, error, users } = userList;
+  // Getting the list of products from state
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
   //   Get current logged user from state
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
-  const userDelete = useSelector((state) => state.userDelete);
-  const { success: successDelete } = userDelete;
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
-      dispatch(listUsers());
+      dispatch(listProducts());
     } else {
       history.push('/login');
     }
-  }, [dispatch, userInfo, history, successDelete]);
+  }, [dispatch, userInfo, history]);
 
   const deleteHandler = (id) => {
+    //   Delete Product
     if (window.confirm('Are you sure?')) {
-      dispatch(deleteuser(id));
     }
   };
+
+  const createProductHandler = () => {};
   return (
     <>
-      <h1>Users</h1>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Products</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button className='my-3' onClick={createProductHandler}>
+            <i className='fa fa-plus' aria-hidden='true'></i> Create Product
+          </Button>
+        </Col>
+      </Row>
       {loading ? (
         <Loader />
       ) : error ? (
@@ -43,34 +53,22 @@ const UserListScreen = ({ history }) => {
             <tr>
               <th>ID</th>
               <th>Name</th>
-              <th>Email</th>
-              <th>Admin</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Brand</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user) => (
-              <tr key={user._id}>
-                <th>{user._id}</th>
-                <th>{user.name}</th>
-                <th>
-                  <a href={`mailto:${user.email}`}>{user.email}</a>
-                </th>
-                <th>
-                  {user.isAdmin ? (
-                    <i
-                      className='fa fa-check'
-                      style={{ color: 'green' }}
-                      aria-hidden='true'></i>
-                  ) : (
-                    <i
-                      className='fa fa-times'
-                      style={{ color: 'red' }}
-                      aria-hidden='true'></i>
-                  )}
-                </th>
+            {products.map((product) => (
+              <tr key={product._id}>
+                <th>{product._id}</th>
+                <th>{product.name}</th>
+                <th>$ {product.price}</th>
+                <th>{product.category}</th>
+                <th>{product.brand}</th>
                 <td>
-                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/product/${product._id}/edit`}>
                     <Button variant='light' className='btn-sm'>
                       <i className='fas fa-edit    '></i>
                     </Button>
@@ -78,7 +76,7 @@ const UserListScreen = ({ history }) => {
                   <Button
                     variant='danger'
                     className='btn-sm'
-                    onClick={() => deleteHandler(user._id)}>
+                    onClick={() => deleteHandler(product._id)}>
                     <i className='fa fa-trash' aria-hidden='true'></i>
                   </Button>
                 </td>
@@ -91,4 +89,4 @@ const UserListScreen = ({ history }) => {
   );
 };
 
-export default UserListScreen;
+export default ProductListScreen;
