c5aeca1411291440fb154683c7a20cb5c9b75c71
Manuel Rodriguez <manrodri92@gmail.com>
2020-12-30 10:58:45 +0300

    Added backend for placing reviews

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -9,13 +10,18 @@ import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
-const app = express();
-
 // Initialize dotenv
 dotenv.config();
 
 // Connection to DB
 connectDB();
+
+const app = express();
+
+// Run morgan logger in dev Mode
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Parser
 app.use(express.json());
